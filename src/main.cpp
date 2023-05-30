612239eab34d04ec47f7fca339a0e12d49e5aa36
Volodymyr Zabila <wzabila1995@gmail.com>
2023-05-30 11:31:24 +0300

    Added qml code

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:src/qml/main.qml")));
 
     if (engine.rootObjects().isEmpty())
         return -1;
